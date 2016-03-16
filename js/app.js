175c23c06e78e1c04d7f930d9ae6231d95acf882
Gloria15 Manenti <Gloria15 Manenti>
2016-03-16 01:31:48 +0100

    adding all files

---
+++ js/app.js
@@ -0,0 +1,38 @@
+
+// Capture the click event on a link to ann image
+var $overlay= $('<div id="overlay"></div>');
+var $image = $("<img>");
+var $caption=$("<p></p>")
+$overlay.append($image);
+$overlay.append($caption);
+console.log($overlay)
+$("section").append($overlay);
+$("#gallery a").click(function(event){
+	event.preventDefault();
+var imageLocation = $(this).attr("href");
+var imageCaption= $(this).children("img").attr("alt");
+$image.attr("src",imageLocation);
+$caption.text(imageCaption);
+$overlay.show();
+$("ul").hide();
+$("h3").hide();
+$("footer").hide();
+$("p").hide();
+
+});
+	//show the overaly
+	//update the overlay with the image linked in the link
+	//get child's alt attribute and set caption
+
+//add overlay
+ 	//an image
+ 	// a caption
+// when overlay is clicked 
+$overlay.click(function(){
+$(this).hide();
+$("ul").show();
+$("h3").show();
+$("footer").show();
+$("p").show();
+})
+ 	//hide the overaly
